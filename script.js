dd71c65924dbb547012e94c53f33b7bf0bb7691e
HCC21 <hcchcchcc08@gmail.com>
2026-02-20 23:21:21 +0800

    Add files via upload
    Signed-off-by: HCC21 <hcchcchcc08@gmail.com>

--- script.js
+++ script.js
@@ -235,8 +235,58 @@ let currentIndex = -1;
 let songs = [];
 
 /* ============================
-   🎵 生成 playlist
+   🎵 自動生成 Playlist
 ============================ */
+const songsData = [
+    { name: "一千個願意", src: "music/一千個願意.mp3", cover: "covers/cover9.jpg", cat: "slow songs" },
+    { name: "遲來的春天", src: "music/遲來的春天.mp3", cover: "covers/cover3.jpg", cat: "slow songs" },
+    { name: "感情的段落", src: "music/感情的段落.mp3", cover: "covers/cover1.jpg", cat: "female" },
+    { name: "愛情是一種法國甜品", src: "music/愛情是一種法國甜品.mp3", cover: "covers/cover6.jpg", cat: "female" },
+    { name: "痛哭", src: "music/痛哭.mp3", cover: "covers/cover2.jpg", cat: "slow songs" },
+    { name: "最後的信仰", src: "music/最後的信仰.mp3", cover: "covers/cover5.jpg", cat: "female" },
+    { name: "雪中情", src: "music/雪中情.mp3", cover: "covers/cover8.jpg", cat: "slow songs" },
+    { name: "迷戀", src: "music/迷戀.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "記得", src: "music/記得.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "真情流露", src: "music/真情流露.mp3", cover: "covers/cover2.jpg", cat: "slow songs" },
+    { name: "相對無言", src: "music/相對無言.mp3", cover: "covers/cover4.jpg", cat: "slow songs" },
+    { name: "為何仍剩我一人", src: "music/為何仍剩我一人.mp3", cover: "covers/cover2.jpg", cat: "slow songs" },
+    { name: "柔情蜜意", src: "music/柔情蜜意.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "明目張膽", src: "music/明目張膽.mp3", cover: "covers/cover3.jpg", cat: "female" },
+    { name: "我是你未來", src: "music/我是你未來.mp3", cover: "covers/cover7.jpg", cat: "slow songs" },
+    { name: "吻感", src: "music/吻感.mp3", cover: "covers/cover6.jpg", cat: "slow songs" },
+    { name: "你狠心來傷我嗎", src: "music/你狠心來傷我嗎.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "再渡艷陽天", src: "music/再渡艷陽天.mp3", cover: "covers/cover2.jpg", cat: "female" },
+    { name: "心有獨鍾(鋼琴版)", src: "music/心有獨鍾(鋼琴版).mp3", cover: "covers/cover3.jpg", cat: "slow songs" },
+    { name: "不要哭了", src: "music/不要哭了.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "千年女王", src: "music/千年女王.mp3", cover: "covers/cover3.jpg", cat: "kids" },
+    { name: "千年女王(傳說)", src: "music/千年女王(傳說).mp3", cover: "covers/cover5.jpg", cat: "kids" },
+    { name: "飄零燕", src: "music/飄零燕.mp3", cover: "covers/cover8.jpg", cat: "kids" },
+    { name: "1874", src: "music/1874.mp3", cover: "covers/cover5.jpg", cat: "slow songs" },
+    { name: "Sol4", src: "music/Sol4.mp3", cover: "covers/cover3.jpg", cat: "slow songs" },
+    { name: "一憶三千八天", src: "music/一憶三千八天.mp3", cover: "covers/cover2.jpg", cat: "slow songs" },
+    { name: "不見不散", src: "music/不見不散.mp3", cover: "covers/cover4.jpg", cat: "slow songs" },
+    { name: "你給我自信", src: "music/你給我自信.mp3", cover: "covers/cover1.jpg", cat: "fast songs" },
+    { name: "告訴我你會在夢境中等我", src: "music/告訴我你會在夢境中等我.mp3", cover: "covers/cover7.jpg", cat: "slow songs" },
+    { name: "我心不死", src: "music/我心不死.mp3", cover: "covers/cover2.jpg", cat: "female" },
+    { name: "我的親愛還是你", src: "music/我的親愛還是你.mp3", cover: "covers/cover6.jpg", cat: "slow songs" },
+    { name: "我這樣愛你", src: "music/我這樣愛你.mp3", cover: "covers/cover7.jpg", cat: "slow songs" },
+    { name: "我愛玫瑰園", src: "music/我愛玫瑰園.mp3", cover: "covers/cover8.jpg", cat: "fast songs" },
+    { name: "沒有你的愛", src: "music/沒有你的愛.mp3", cover: "covers/cover1.jpg", cat: "slow songs" },
+    { name: "初戀", src: "music/初戀.mp3", cover: "covers/cover5.jpg", cat: "female" },
+    { name: "送曲送給你", src: "music/送曲送給你.mp3", cover: "covers/cover7.jpg", cat: "slow songs" },
+    { name: "送你一瓣的雪花", src: "music/送你一瓣的雪花.mp3", cover: "covers/cover3.jpg", cat: "slow songs" },
+    { name: "假的戀愛", src: "music/假的戀愛.mp3", cover: "covers/cover4.jpg", cat: "female" },
+    { name: "富士山下", src: "music/富士山下.mp3", cover: "covers/cover3.jpg", cat: "slow songs" },
+    { name: "棉胎", src: "music/棉胎.mp3", cover: "covers/cover6.jpg", cat: "female" },
+    { name: "無心快語", src: "music/無心快語.mp3", cover: "covers/cover7.jpg", cat: "fast songs" },
+    { name: "給自己的情書", src: "music/給自己的情書.mp3", cover: "covers/cover4.jpg", cat: "female" },
+    { name: "媽咪與天父", src: "music/媽咪與天父.mp3", cover: "covers/cover8.jpg", cat: "festival" },
+    { name: "暸解你的所有", src: "music/暸解你的所有.mp3", cover: "covers/cover2.jpg", cat: "slow songs" },
+    { name: "離開請關燈", src: "music/離開請關燈.mp3", cover: "covers/cover3.jpg", cat: "female" },
+    { name: "魔法奇緣之媽媽知道", src: "music/魔法奇緣之媽媽知道.mp3", cover: "covers/cover5.jpg", cat: "kids" }
+];
+
+// 生成 playlist
 function generatePlaylist() {
     playlist.innerHTML = "";
     songsData.forEach((song, index) => {
