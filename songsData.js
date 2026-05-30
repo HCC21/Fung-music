5a52238ba7c5e6793911586997f9d13373d49c2e
HCC21 <hcchcchcc08@gmail.com>
2026-05-30 23:16:10 +0800

    Add files via upload
    Signed-off-by: HCC21 <hcchcchcc08@gmail.com>

--- songsData.js
+++ songsData.js
@@ -364,7 +364,7 @@ const songsData = [
   
 },{
     name: "有你明白我(敏唱)",
-    src: "music/manman/有你明白我(敏唱).mp3",
+    src: "music/manman/有你明白我(敏唱).Mp3",
     cover: "covers/man4.jpg",
     cat: "man",
   
@@ -983,7 +983,7 @@ const songsData = [
 },
 
 { name: "最美的第七天敏",
-  src: "music/manman/最美的第七天敏.mp3",
+  src: "music/manman/最美的第七天敏.Mp3",
   cover: "covers/man4.jpg",
   cat: "man",
  
